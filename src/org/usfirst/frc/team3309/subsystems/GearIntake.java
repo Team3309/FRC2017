d22f4ec5732dee95b2f6c7a97a7dde9128fa9e08
Matthew Krager <metoofarmer@yahoo.com>
2017-01-23 19:59:25 -0800

    Climber and GearIntake

---
+++ src/org/usfirst/frc/team3309/subsystems/GearIntake.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team3309.subsystems;
+
+import org.team3309.lib.KragerSystem;
+
+public class GearIntake extends KragerSystem {
+
+	public GearIntake(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void updateTeleop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateAuto() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initTeleop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initAuto() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendToSmartDash() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void manualControl() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
