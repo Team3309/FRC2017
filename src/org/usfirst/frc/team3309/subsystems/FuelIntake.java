cecbf7e8f857b6365c48298fd135dbdc9e7a3c4b
Matthew Krager <metoofarmer@yahoo.com>
2017-01-24 20:05:15 -0800

    Intake for fuel controlled by triggers

--- src/org/usfirst/frc/team3309/subsystems/FuelIntake.java
+++ src/org/usfirst/frc/team3309/subsystems/FuelIntake.java
@@ -2,8 +2,13 @@ package org.usfirst.frc.team3309.subsystems;
 
 import org.team3309.lib.ControlledSubsystem;
 import org.team3309.lib.controllers.statesandsignals.InputState;
+import org.usfirst.frc.team3309.driverstation.Controls;
 
-public class FuelIntake extends ControlledSubsystem{
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
+public class FuelIntake extends ControlledSubsystem {
+
+	private double MIN_VALUE_TO_MOVE = .15;
 
 	public FuelIntake(String name) {
 		super(name);
@@ -12,14 +17,27 @@ public class FuelIntake extends ControlledSubsystem{
 
 	@Override
 	public void updateTeleop() {
-		// TODO Auto-generated method stub
-		
+		double driverRightTrigger = Controls.driverController.getTriggerAxis(Hand.kRight);
+		double driverLeftTrigger = Controls.driverController.getTriggerAxis(Hand.kLeft);
+		double operatorRightTrigger = Controls.operatorController.getTriggerAxis(Hand.kRight);
+		double operatorLeftTrigger = Controls.operatorController.getTriggerAxis(Hand.kLeft);
+		if (driverRightTrigger > MIN_VALUE_TO_MOVE) {
+			setFuelIntake(driverRightTrigger);
+		} else if (driverLeftTrigger > MIN_VALUE_TO_MOVE) {
+			setFuelIntake(-driverLeftTrigger);
+		} else if (operatorRightTrigger > MIN_VALUE_TO_MOVE) {
+			setFuelIntake(operatorRightTrigger);
+		} else if (operatorLeftTrigger > MIN_VALUE_TO_MOVE) {
+			setFuelIntake(-operatorLeftTrigger);
+		} else {
+			setFuelIntake(0);
+		}
 	}
 
 	@Override
 	public void updateAuto() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -31,24 +49,28 @@ public class FuelIntake extends ControlledSubsystem{
 	@Override
 	public void sendToSmartDash() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void manualControl() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void initTeleop() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void initAuto() {
 		// TODO Auto-generated method stub
+
+	}
+
+	public void setFuelIntake(double power) {
 		
 	}
 
