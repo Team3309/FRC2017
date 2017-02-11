d01c02b6b0ab8239d04294559f83b99a4c39ff10
Matthew Krager <metoofarmer@yahoo.com>
2017-02-11 11:08:41 -0800

    changed to init and update
    now it auto checks if its teleop or auto

--- src/org/usfirst/frc/team3309/robot/Systems.java
+++ src/org/usfirst/frc/team3309/robot/Systems.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.team3309.lib.KragerSystem;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class Systems {
 	private static List<KragerSystem> systems = new LinkedList<KragerSystem>();
 
@@ -12,25 +14,24 @@ public class Systems {
 		systems.add(e);
 	}
 
-	public static void updateTeleop() {
-		for (KragerSystem e : systems)
-			e.updateTeleop();
+	public static void update() {
+		for (KragerSystem e : systems) {
+			if (DriverStation.getInstance().isAutonomous())
+				e.updateAuto();
+			else
+				e.updateTeleop();
+		}
 	}
 
-	public static void upateAuto() {
-		for (KragerSystem e : systems)
-			e.updateAuto();
+	public static void init() {
+		for (KragerSystem e : systems) {
+			if (DriverStation.getInstance().isAutonomous())
+				e.initAuto();
+			else
+				e.initAuto();
+		}
 	}
 
-	public static void initTeleop() {
-		for (KragerSystem e : systems)
-			e.initTeleop();
-	}
-
-	public static void initAuto() {
-		for (KragerSystem e : systems)
-			e.initAuto();
-	}
 
 	public static void manualControl() {
 		for (KragerSystem e : systems)
